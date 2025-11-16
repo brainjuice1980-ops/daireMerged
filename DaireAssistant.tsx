7b9ce047b4957f0e947cc601a88f22cec47f556e
karwasanjay007 <karwasanjay007@gmail.com>
2025-11-16 18:25:36 +0000

    baseline skarwa

--- DaireAssistant.tsx
+++ DaireAssistant.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import DaireApp from './DaireApp';
+import './DaireApp.css';
 
 const DaireAssistant: React.FC = () => {
   return (
-    <div className="w-full h-full">
+    <div className="daire-map-assistant-container">
       <DaireApp />
     </div>
   );
